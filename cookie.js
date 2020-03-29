bd223d971f050af7882aa8436fd2debbbbe528a2
Rodger Rüdiger <ruediger@webit.de>
2020-03-29 17:12:23 +0200

    [TASK] code style

--- cookie.js
+++ cookie.js
@@ -7,7 +7,7 @@ define(function() {
   @see http://www.quirksmode.org/js/cookies.html
   *************************************************************/
   var Cookie = {
-    create: function(name,value, days, sameSite, domain) {
+    create: function(name, value, days, sameSite, domain) {
       var expires = "";
       var cookie_string = "";
       var sameSite_settings = '';
@@ -16,11 +16,11 @@ define(function() {
 
       if (days) {
         var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        expires = "; expires="+date.toGMTString();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
       }
 
-      cookie_string = name+"="+value+expires+"; path=/";
+      cookie_string = name + "=" + value + expires + "; path=/";
 
       if (domain) {
         cookie_string += "; domain=" + domain;
@@ -42,18 +42,30 @@ define(function() {
 
       document.cookie = cookie_string;
     },
+
+    /**
+     * Read Cookie.
+     * @function read
+     * @public
+     */
     read: function(name) {
       var nameEQ = name + "=";
       var ca = document.cookie.split(';');
-      for(var i=0;i < ca.length;i++) {
+      for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
       }
       return null;
     },
+
+    /**
+     * Erase cookie.
+     * @function erase
+     * @public
+     */
     erase: function(name) {
-      this.createCookie(name,'',-1);
+      this.createCookie(name, '', -1);
     }
   };
 
